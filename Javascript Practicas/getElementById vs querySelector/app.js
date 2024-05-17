630d46d207f2a773b55f21d002738643e50f49f8
Hector-45 <opezrodriguez45@gmail.com>
2024-05-17 14:34:33 -0600

    update

---
+++ Javascript Practicas/getElementById vs querySelector/app.js
@@ -0,0 +1,9 @@
+/*Marca todo el DOOM */
+//console.log(document);
+
+/**Forma basica para acceder por el id*/
+//const element = document.getElementById("card-1");
+
+//Acceder a propiedades
+//console.dir(element);
+
