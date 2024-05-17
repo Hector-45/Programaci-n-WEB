1ceb9fe5619b91a6630f7ed1c02de68f41393dbb
Hector-45 <opezrodriguez45@gmail.com>
2024-05-17 17:02:37 -0600

    update

--- Javascript Practicas/carrito de compra/app.js
+++ Javascript Practicas/carrito de compra/app.js
@@ -1,4 +1,5 @@
-/**querySelector es un método que acepta el 
+/**querySelector 
+ * es un método que acepta el 
  * selector exacto del CSS en una cadena y 
  * retorna un elemento. Puedes usarlo para 
  * seleccionar los colores rojo y negro así 
@@ -10,4 +11,10 @@ const blackColor = document.querySelector(".black");
 const imageCard = document.querySelector(".product-image");
 const feedbackBtn = document.querySelector(".feedback");
 
+/**
+ * getElementsByClassName
+ * Puedes usar este selector para seleccionar el color gris. Es muy similar 
+ * al querySelector. La única diferencia es que este método acepta solo el 
+ * nombre de la clase, sin el punto anterior (.)
+ */
 const grayColor = document.getElementsByClassName("gray");
