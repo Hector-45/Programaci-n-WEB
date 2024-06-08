f3c1c8e09822893ed232f0e120bada14bf08fb54
Hector-45 <opezrodriguez45@gmail.com>
2024-06-07 22:22:15 -0600

    update laboratorio bootstrap

--- Ejercicios y laboratorios/Tarea 7 - BackEnd FrontEnd/frontend/listarArticulos.js
+++ Ejercicios y laboratorios/Tarea 7 - BackEnd FrontEnd/frontend/listarArticulos.js
@@ -12,8 +12,10 @@ fetch(url)
         
         data.clientes.forEach(row => {
             body+=`
-            <tr><td>${row.codCliente}</td><td>${row.Nombre}</td><td>${row.apellidos}</td><td>${row.empresa}</td><td>${row.puesto}</td><td>${row.CP}</td><td>${row.provincia}</td><td>${row.telefono}</td><td>${row.fechaNacimiento}</td><td><button type="button" class="btn btn-primary">Editar</button></td><td><button type="button" class="btn btn-danger">Eliminar</button></td></tr>`
+            <tr><td>${row.codCliente}</td><td>${row.Nombre}</td><td>${row.apellidos}</td><td>${row.empresa}</td><td>${row.puesto}</td><td>${row.CP}</td><td>${row.provincia}</td><td>${row.telefono}</td><td>${row.fechaNacimiento}</td><td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Editar</button></td><td><button type="button" class="btn btn-danger">Eliminar</button></td></tr>`
         });
+
+        
         //for(let i = 0 ; i<data.clientes.length; i++){
         //    body+=`
          //   <tr><td>${data.codArticulo}</td><td>${data.Nombre}</td><td>${data.Descripcion}</td><td>${data.imagen}</td><td>${data.precioUnidad}</td><td>${data.stockSeguridad}</td><td>${data.unidadesStock}</td></tr>`
